6994415d38df8141c3d0a535fa3d1cba6abf3907
sermalp <sermalp@yandex.ru>
2022-02-20 20:05:50 +0300

    добавлен шаблон Flask

--- app.py
+++ app.py
@@ -4,39 +4,39 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route('/search', methods=['GET', 'POST'])
 def getData():
-    print("request.method: "+request.method)
     if request.method == 'POST':
-        adress = request.form.get('adress')
-        filtr = 'mp4'
-        list = getListVideoFiles(adress, filtr)
-        return getTextHTML()+'<h1>Файлы из каталога: '+adress+'</h1>'+'<p>'+list+'</p>'
-    
-    #return 'Поломка!'
+        dict_data = {}
+        dict_data['adress'] = request.form.get('adress')
+        dict_data['list_video'] = getListVideoFiles(dict_data['adress'], 'mp4')
+        dict_data['title'] = 'Страница списка видео'
+        return render_template("html/block_video.html", data=dict_data)
+
+
+@app.route('/')
+def mainPage():
+    dict_data = {}
+    dict_data['title'] = 'Страница списка видео'
+    return render_template("html/index.html", data=dict_data)#return getTextHTML()
+
 
 def getListVideoFiles(adress, filtr=''):
-    #Ваш код на Python для отправления сообщений в Telegram через бота
-    print(adress)
-    #file_list = os.listdir()  # список файлов и папок в директории, где запущена программа
-    #file_list = os.listdir('.')  # синоним
-    #file_list = os.listdir(adress)  # список имен файлов и папок в данной папке
-    strList = ''
+    #формирует список видео файлов в каталоге
+    strList = []
+    dictVideoFile = {}
     try:
         for file in os.scandir(adress):
-        #for file in os.listdir(adress):
             if file.name.endswith(filtr):
-                strList = strList + file.path +'<br />'
+                dictVideoFile['path'] = file.path
+                dictVideoFile['name'] = file.name
+                strList.append(dictVideoFile)
+                
     except:
         Warning
         strList = 'Каталога с таким адрес не существует!'
 
     return strList
-
-@app.route('/')
-def mainPage():
-    return render_template("html/index.html")#return getTextHTML()
 
 
 def getTextHTML():
